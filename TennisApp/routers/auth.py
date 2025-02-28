5144c69818aa8e85ab73e8c6b0984afb96cc3695
Amit Shokeen <ashokeen@Amits-MacBook-Pro.local>
2025-02-28 14:12:44 +1100

    minor change

--- TennisApp/routers/auth.py
+++ TennisApp/routers/auth.py
@@ -1,5 +1,5 @@
 from datetime import timedelta, datetime, timezone
-from fastapi import APIRouter, Depends, status, HTTPException, Request
+from fastapi import APIRouter, Depends, status, HTTPException, Request, Cookie
 from pydantic import BaseModel
 from ..models import User
 from passlib.context import CryptContext
@@ -72,7 +72,9 @@ def create_access_token(username: str, user_id: int, role: str, expires_delta: t
     encode.update({'exp': expires})
     return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
 
-async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
+async def get_current_user(
+        token: Annotated[str, Depends(oauth2_bearer)]
+    ):
     try: 
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         username: str = payload.get('sub')
