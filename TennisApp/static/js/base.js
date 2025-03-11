3fb2f60bfc043c8b26f24c661410e674b8ebd347
Amit Shokeen <ashokeen@Amits-MacBook-Pro.local>
2025-03-12 09:01:04 +1100

    terms and conditions, refactored code, improved register form

--- TennisApp/static/js/base.js
+++ TennisApp/static/js/base.js
@@ -44,37 +44,6 @@ if (loginForm) {
     });
 }
 
-// Registeration form
-// const registerForm = document.getElementById('registerForm');
-// if (registerForm) {
-//     registerForm.addEventListener('submit', function(event) {
-//         event.preventDefault();
-//         const password = document.getElementById('password').value;
-//         const confirmPassword = document.getElementById('confirmPassword').value;
-//         const errorDiv = document.getElementById('error');
-        
-//         if (password !== confirmPassword) {
-//             errorDiv.textContent = 'Passwords do not match!';
-//             return;
-//         }
-
-//         // Collect form data
-//         const userData = {
-//             email: document.getElementById('email').value,
-//             username: document.getElementById('username').value,
-//             firstName: document.getElementById('firstName').value,
-//             lastName: document.getElementById('lastName').value,
-//             role: document.getElementById('role').value,
-//             apartmentNumber: document.getElementById('apartmentNumber').value,
-//             phoneNumber: document.getElementById('phoneNumber').value,
-//             password: password
-//         };
-
-//         console.log('User Registered:', userData);
-//         alert('Registration successful!');
-//     });
-// }
-
 // Logout form
 function logout() {
     // Get all cookies
